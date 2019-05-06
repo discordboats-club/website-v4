d9f4801896c5321328b5b53d63b7bf0876823320
jellz <danielgulic@gmail.com>
2019-05-06 20:40:42 +1000

    use keys/ directory for keys

--- server/gen_jwt.sh
+++ server/gen_jwt.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+mkdir keys && cd keys
 ssh-keygen -t rsa -b 4096 -f jwt.key
 openssl rsa -in jwt.key -pubout -outform PEM -out jwt.key.pub
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var app = express();
 
 // constants
 var r = module.exports.r = require('rethinkdbdash')({ db: 'discordboatsclub_v4', port: process.env.RETHINKDB_PORT || 28015, host: process.env.RETHINKDB_HOST || 'localhost' });
-const JWT_KEY = module.exports.JWT_KEY = fs.readFileSync('jwt.key').toString();
+const JWT_KEY = module.exports.JWT_KEY = fs.readFileSync('keys/jwt.key').toString();
 const PORT = process.env.PORT || 3001;
 
 var client = require('./client.js');
